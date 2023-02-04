bf523c966779daba9a6dafafa165683b12b16ab7
hilminugroho12 <hilminugroho12@gmail.com>
2023-02-04 22:08:01 +0700

    init

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+/*
+---------------------------------------------
+Developed by IPAN
+Theme by: JANUX (Dennis Ji)
+---------------------------------------------
+*/
+mysql_select_db('db_tibsc',  mysql_connect('localhost','root',''))or die(mysql_error());
+?>
